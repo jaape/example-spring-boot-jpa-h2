44cef3edac8a1acf46ed8a5ceceb2542d0f585ba
Jaakko Ala-Luhtala <ja.pe@outlook.com>
2019-11-24 21:51:23 +0200

    Remove flyway-config Flyway didn't sit right with this project. I wanted to steer clear of SQL

--- src/main/resources/db/migration/V1.1__Initialize.sql
+++
@@ -1,8 +0,0 @@
-create table car
-(
-	id serial,
-	make varchar(50),
-	model varchar(50)
-);
-
-create unique index car_id_uindex on car (id);
--- src/main/resources/db/migration/V1.2__Populate.sql
+++
@@ -1,2 +0,0 @@
-insert into car (make, model) values ('Audi', '100');
-insert into car (make, model) values ('Skoda', 'Octavia combi');
