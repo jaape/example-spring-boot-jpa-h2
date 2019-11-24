6fb1d97607ea398ba747745a6ca3470819bab21c
Jaakko Ala-Luhtala <ja.pe@outlook.com>
2019-11-24 14:19:12 +0000

    Add H2 database Car.java is an example Entity. The table is automatically created when the software launches

---
+++ src/main/java/fi/exampleapplication/model/entity/Car.java
@@ -0,0 +1,14 @@
+package fi.exampleapplication.model.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Car {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String make;
+    private String model;
+}
